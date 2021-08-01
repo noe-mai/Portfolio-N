71c91bf0a3cb56f82f424bfcab08bfa86cd5e946
noe-mai <noelia.s.maiquez@gmail.com>
2021-07-31 21:37:43 -0300

    pruebas react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, ButtonGroup } from "@chakra-ui/react"
+import { Box, Grid } from "@chakra-ui/react"
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box w="100%" h="200px" bgGradient="linear(to-r, green.200, pink.500)" />
+ <Box w="100%" h="50px" />
+
+ <Box
+  w="100%"
+  h="200px"
+  bgGradient={[
+    "linear(to-tr, teal.300,yellow.400)",
+    "linear(to-t, blue.200, teal.500)",
+    "linear(to-b, orange.100, purple.300)",
+  ]}
+/>
+
+<Box w={1/2} />
+     <Button colorScheme="blue" variant="outline">Button</Button>
+
+     <Grid gap={2} autoFlow="row dense">
+  Grid
+</Grid>
+
+<Box border="1px" borderColor="gray.200">
+  Card
+</Box>
+
+
+<Button borderRightRadius="0">Button 1</Button>
+
+<Button borderLeftRadius="0">Button 2</Button>
+
+<Button borderTopRadius="md">Button 2</Button>
+
     </div>
   );
 }
