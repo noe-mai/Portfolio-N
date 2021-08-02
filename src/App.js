160406b64bb7858ab9e807f9ccdfcf018bc56946
noe-mai <noelia.s.maiquez@gmail.com>
2021-08-01 23:24:18 -0300

    primeros componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+
 import { Button, ButtonGroup } from "@chakra-ui/react";
 import { Box, Grid } from "@chakra-ui/react";
+import { Avatar, AvatarBadge, AvatarGroup } from "@chakra-ui/react";
 import Contacto from "./Components/Contacto/Contacto";
 import Home from "./Components/Home/Home";
 import Footer from "./Components/Footer/Footer";
@@ -10,8 +12,10 @@ import Navbar from "./Components/Navbar/Navbar";
 function App() {
   return (
     <div>
-      <Box w="100%" h="200px" bgGradient="linear(to-r, green.200, pink.500)" />
+ <Box w="100%" h="200px" bgGradient="linear(to-r, green.200, pink.500)" />
  <Box w="100%" h="50px" />
+
+ <Navbar />
 
  <Box
   w="100%"
@@ -22,6 +26,21 @@ function App() {
     "linear(to-b, orange.100, purple.300)",
   ]}
 />
+
+
+<Box
+  as="button"
+  p={4}
+  color="white"
+  fontWeight="bold"
+  borderRadius="md"
+  bgGradient="linear(to-r, teal.500,green.500)"
+  _hover={{
+    bgGradient: "linear(to-r, red.500, yellow.500)",
+  }}
+>
+  Click here
+</Box>
 
 <Box w={1/2} />
      <Button colorScheme="blue" variant="outline">Button</Button>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,2 +1,22 @@
 import React from "react";
 import { Box, Grid } from "@chakra-ui/react";
+
+
+
+const Navbar = () => {
+
+    return(
+
+ <Box bg="grey" w="100%" p={4} color="white">
+  Futuro Navbar prueba componente
+</Box>
+    );
+ 
+<Box
+  w="100%"
+  h="200px"
+  bgGradient="linear(red.100 0%, orange.100 25%, yellow.100 50%)"
+/>
+}
+
+export default Navbar;
