6b107d6c660f58cc2fcf9b46403e8535d333bcfb
noe-mai <noelia.s.maiquez@gmail.com>
2021-08-06 01:07:02 -0300

    mas pruebas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import {Button, ButtonGroup} from '@chakra-ui/react';
+import './App.css';
 import {Box, Grid} from '@chakra-ui/react';
-import {Image} from '@chakra-ui/react';
-import {Avatar, AvatarBadge, AvatarGroup} from '@chakra-ui/react';
 import Contacto from './Components/Contacto/Contacto';
 import Home from './Components/Home/Home';
 import Footer from './Components/Footer/Footer';
@@ -13,14 +11,10 @@ import CustomButton from './Components/CustomButton/Button';
 function App() {
 	return (
 		<div>
-			<Box w='100%' h='200px' bgGradient='linear(to-r, green.200, pink.500)' />
-			<Box w='100%' h='50px' />
-
 			<Navbar />
 			<Home />
 			<Footer />
-			<Skills />
-			<Intro />
+			<Contacto />
 			<CustomButton texto='click ' color='yellow' />
 			<CustomButton texto='hola soy otro boton' />
 
@@ -33,20 +27,6 @@ function App() {
 					'linear(to-b, orange.100, purple.300)',
 				]}
 			/>
-
-			<Box
-				as='button'
-				p={4}
-				color='white'
-				fontWeight='bold'
-				borderRadius='md'
-				bgGradient='linear(to-r, teal.500,green.500)'
-				_hover={{
-					bgGradient: 'linear(to-r, red.500, yellow.500)',
-				}}
-			>
-				Click here
-			</Box>
 		</div>
 	);
 }
