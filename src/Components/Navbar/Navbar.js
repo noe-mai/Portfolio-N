20368bd23adf2d2875b2b7e7bddb41de176afe65
noe-mai <noelia.s.maiquez@gmail.com>
2021-08-11 00:27:37 -0300

    agregar hover en el logo

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import styled from '@emotion/styled';
 
 import {Box, Container, Avatar, Flex, HStack} from '@chakra-ui/react';
 
 import logo from '../../Images/logo.webp';
 
+const Logo = styled.div`
+	transition: transform 0.3s ease-in;
+
+	&:hover {
+		transform: rotate(180deg);
+	}
+`;
+
 const Navbar = () => {
 	return (
 		<Container maxW='1240px' style={{position: 'sticky'}}>
 			<Flex p='20px' mb='30px' direction='row' justify='space-between'>
-				<Link to='/'>
-					<Avatar src={logo} />
-				</Link>
+				<Logo size={2}>
+					<Link to='/'>
+						<Avatar src={logo} />
+					</Link>
+				</Logo>
 
 				<HStack spacing='20px'>
 					<Box>
